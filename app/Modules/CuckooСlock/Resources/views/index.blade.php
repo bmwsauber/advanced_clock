d7f6d733d31d4f3c0c6679490fd1c330e03ac4a7
Alex Du <alexandr.dubovis@gmail.com>
2021-01-06 09:40:17 +0200

    Init Laravel 8 + tailwindcss + Nwidart Modules

---
+++ app/Modules/CuckooСlock/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('cuckooСlock::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('cuckooСlock.name') !!}
+    </p>
+@endsection
